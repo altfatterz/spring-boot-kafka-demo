5f6f2d4a666c933a0087ffcfee299ef55eaf76b7
Zoltan Altfatter <altfatterz@gmail.com>
2019-08-14 22:45:05 +0200

    initial avro kafka

---
+++ events-transformer/README.md
@@ -0,0 +1,9 @@
+```bash
+$ mvn generate-sources
+```
+
+error:  Missing required configuration "schema.registry.url" which has no default value.
+
+
+# Reference
+https://medium.com/@sunilvb/spring-boot-kafka-schema-registry-c6e32485a7c8
---
+++ events-transformer/src/main/java/com/example/EventsTransformerApplication.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import com.example.avro.Movie;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class EventsTransformerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsTransformerApplication.class, args);
+	}
+
+}
+
+@Service
+@Slf4j
+class MovieEventsTransformer {
+
+	private static final String TOPIC = "movies";
+
+	@KafkaListener(topics = TOPIC)
+	public void consume(Movie movie) {
+		log.info("consumed movie:{} ", movie);
+	}
+}
